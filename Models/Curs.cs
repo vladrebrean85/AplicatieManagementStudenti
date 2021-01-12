417f63f0b7e9c99256c992d43bac9be9e87c9c0c
Vlad Rebreanu <vlad.rebreanu@gmail.com>
2021-01-12 17:32:20 +0200

    Adaugare clase pt DB

---
+++ Models/Curs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplicatieManagementStudenti.Models
+{
+    public class Curs
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CursID { get; set; }
+        public string Titlu { get; set; }
+        public int Credite { get; set; }
+
+        public ICollection<Inscriere> Inscrieri { get; set; }
+    }
+}
